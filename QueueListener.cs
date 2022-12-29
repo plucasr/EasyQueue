a70139d7318e4934a2776a6cba401a492689e7f9
Lucas Pedroso <webdevlucas@gmail.com>
2022-12-29 14:36:07 -0400

    readme file add

--- QueueListener.cs
+++ QueueListener.cs
@@ -2,10 +2,19 @@ namespace HailaQueue;
 
 public class QueueListener
 {
-    public delegate void ItemProcessor<PayloadType>(PayloadType item);
-
-    public Task Processor<ProcessorType>(string processorChannel, ProcessorType payload)
+    public Task Processor<ProcessorType>(
+        string processorChannel,
+        ProcessorType payload,
+        Action<ProcessorType> action
+    )
     {
         return Task.FromResult(0);
     }
+
+    public Task Processor(string processorChannel, string json, Action<string> action)
+    {
+        return Task.FromResult(0);
+    }
+
+    private void Process() { }
 }
---
+++ Readme.md
@@ -0,0 +1,9 @@
+# HailaQueue
+
+## Requisites
+
+1. Runtime + SDK [Net 6](https://dotnet.microsoft.com/en-us/download/dotnet/6.0) - (required).
+
+```bash
+dotnet --version
+```
